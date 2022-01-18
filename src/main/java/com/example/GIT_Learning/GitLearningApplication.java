ab33d868ad02d48820391a521dd82b9fd41a4924
madhuri1997 <46654088+madhuri1997@users.noreply.github.com>
2022-01-18 12:51:52 +0530

    Update GitLearningApplication.java

--- src/main/java/com/example/GIT_Learning/GitLearningApplication.java
+++ src/main/java/com/example/GIT_Learning/GitLearningApplication.java
@@ -10,6 +10,7 @@ public class GitLearningApplication {
 		System.out.println("My application");
 		System.out.println("Branch1 - I always wonder");
 		System.out.println("Its over");
+		System.out.println("Its new");
 		SpringApplication.run(GitLearningApplication.class, args);
 	}
 	
