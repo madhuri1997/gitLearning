fa68b9b596b253d61f19dac831c95488712e7c57
madhuri <madhurireddy60795@gmail.com>
2022-01-18 12:17:51 +0530

    First commit

---
+++ src/main/java/com/example/GIT_Learning/GitLearningApplication.java
@@ -0,0 +1,13 @@
+package com.example.GIT_Learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitLearningApplication.class, args);
+	}
+
+}
